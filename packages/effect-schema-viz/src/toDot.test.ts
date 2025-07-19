42a1b1571cfd84af4b04a1ad93a6f788b1aa0a79
middle-ages <ran@haag59.com>
2025-07-19 14:33:28 -0700

    a

--- packages/effect-schema-viz/src/toDot.test.ts
+++ packages/effect-schema-viz/src/toDot.test.ts
@@ -1,4 +1,5 @@
 import {Struct} from '#annotations'
+import {surround} from '#util'
 import {Schema} from 'effect'
 import {schemasToDot} from './toDot.js'
 
@@ -16,73 +17,67 @@ const AnonymousStructSchema = Schema.Struct({anonymous: Schema.Number})
 
 describe('schemasToDot', () => {
   test('Named struct', () => {
-    expect(iut(NamedStructSchema)).toBe(`digraph "diagram" {
-  "NamedStruct" [
-    label = "NamedStruct";
-  ];
-}`)
+    expect(iut(NamedStructSchema)).toBe(
+      diagram(`  "NamedStruct" [\n    label = "NamedStruct";\n  ];`),
+    )
   })
 
   test('Class', () => {
-    expect(iut(ClassSchema)).toBe(`digraph "diagram" {
-  "ClassSchema" [
-    label = <<table cellspacing="0" cellpadding="0" border="0">
-<tr><td colspan="3" align="center" border="1" sides="B">ClassSchema</td></tr>
-<tr><td colspan="1" align="left">id:</td>
-<td colspan="1" align="left"> </td>
-<td colspan="1" align="left">number</td></tr>
-</table>>;
-  ];
-}`)
+    expect(iut(ClassSchema)).toBe(
+      diagram(`  "ClassSchema" [\n    label = <${table}>;\n  ];`),
+    )
   })
 
   test('Named struct and class', () => {
-    expect(iut(NamedStructSchema, ClassSchema)).toBe(`digraph "diagram" {
-  "NamedStruct" [
+    expect(iut(NamedStructSchema, ClassSchema)).toBe(
+      diagram(`  "NamedStruct" [
     label = "NamedStruct";
   ];
   "ClassSchema" [
-    label = <<table cellspacing="0" cellpadding="0" border="0">
+    label = <${table}>;
+  ];`),
+    )
+  })
+
+  test('No object types', () => {
+    expect(iut(Schema.Number, Schema.String)).toBe(
+      diagram(`  "ERROR: no object types found" [
+    label = "ERROR: no object types found";
+    color = "red";
+    shape = "box";
+  ];`),
+    )
+  })
+
+  test('Anonymous struct', () => {
+    expect(iut(AnonymousStructSchema)).toBe(
+      diagram(`  "ERROR: missing identifier" [
+    label = "ERROR: missing identifier";
+    color = "red";
+    shape = "box";
+  ];`),
+    )
+  })
+
+  test('Anonymous + named structs', () => {
+    expect(iut(AnonymousStructSchema, NamedStructSchema)).toBe(
+      diagram(`  "NamedStruct" [
+    label = "NamedStruct";
+  ];
+  "ERROR: missing identifier" [
+    label = "ERROR: missing identifier";
+    color = "red";
+    shape = "box";
+  ];`),
+    )
+  })
+})
+
+const table = `<table cellspacing="0" cellpadding="0" border="0">
 <tr><td colspan="3" align="center" border="1" sides="B">ClassSchema</td></tr>
 <tr><td colspan="1" align="left">id:</td>
 <td colspan="1" align="left"> </td>
 <td colspan="1" align="left">number</td></tr>
-</table>>;
-  ];
-}`)
-  })
+</table>`
 
-  test('No object types', () => {
-    expect(iut(Schema.Number, Schema.String)).toBe(`digraph "diagram" {
-  "ERROR: no object types found" [
-    label = "ERROR: no object types found";
-    color = "red";
-    shape = "box";
-  ];
-}`)
-  })
-
-  test('Anonymous struct', () => {
-    expect(iut(AnonymousStructSchema)).toBe(`digraph "diagram" {
-  "ERROR: missing identifier" [
-    label = "ERROR: missing identifier";
-    color = "red";
-    shape = "box";
-  ];
-}`)
-  })
-
-  test('Anonymous + named structs', () => {
-    expect(iut(AnonymousStructSchema, NamedStructSchema))
-      .toBe(`digraph "diagram" {
-  "NamedStruct" [
-    label = "NamedStruct";
-  ];
-  "ERROR: missing identifier" [
-    label = "ERROR: missing identifier";
-    color = "red";
-    shape = "box";
-  ];
-}`)
-  })
-})
+const diagram = surround.rest('digraph "diagram" {\n', '\n}')
